20757ae373b5c681c6368a5ee477822832df353d
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-03-05 20:16:33 +0100

    it does something

--- game/android-app/app/src/main/java/gps/tracker/FirstFragment.java
+++ game/android-app/app/src/main/java/gps/tracker/FirstFragment.java
@@ -1,7 +1,11 @@
 package gps.tracker;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,18 +13,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
 import gps.tracker.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private Bitmap bitmap = null;
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
+        System.out.println("onCreateView");
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -33,6 +46,54 @@ public class FirstFragment extends Fragment {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment)
         );
+
+        binding.textView.setText("Hello World");
+        binding.textView.setOnTouchListener((x,d) -> {
+            binding.textView.setText("TEEEEEST");
+            return true;
+        });
+
+        binding.imageView.setOnClickListener(v -> {
+            //binding.imageView.
+            if (bitmap != null) {
+                System.out.println("Bitmap not null");
+                binding.imageView.setImageBitmap(bitmap);
+            }
+            new Thread(this::getImage).start();
+        });
+
+    }
+
+    private synchronized void setBitmap(Bitmap bmp) {
+
+        bitmap = bmp;
+
+        System.out.println("Bitmap set");
+    }
+
+    public void getImage() {
+        System.out.println("getImage");
+        // Make HTTP request to get image
+        // https://picsum.photos/200/300
+
+        try {
+            URL url = new URL("https://picsum.photos/200/300");
+            InputStream in = url.openStream();
+
+            System.out.println("in: " + in);
+            Bitmap myBitmap = BitmapFactory.decodeStream(in);
+
+            System.out.println(myBitmap.getHeight());
+
+            System.out.println("myBitmap: " + myBitmap);
+
+            setBitmap(myBitmap);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
