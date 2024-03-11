a6f789f19ecd61f663455a67fb6393146c63a1e2
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-03-11 16:02:37 +0100

    Change endpoint

--- dashboard/app/src/app.ts
+++ dashboard/app/src/app.ts
@@ -5,24 +5,9 @@ import { renderFileToString } from "../deps.ts";
 const app = opine();
 const router = Router();
 
-const apiProtocol = `http`;
-const websocketProtocol = 'ws';
-const gameServerUrl = '52.158.44.176:8080'
-const websocket = new WebSocket(`${websocketProtocol}://${gameServerUrl}/game`);
-
-websocket.onopen = (e) => {
-  console.log("OPEN", e);
-  websocket.send(
-    JSON.stringify({ "type": ".LoginInfo", "name": "admin", "password": "mud" })
-  );
-}
-websocket.onmessage = (e) => console.log("MESSAGE", e);
-
 
 router.get("/", async (req, res, next) => {
-  let response = await fetch(`${apiProtocol}://${gameServerUrl}/v1/hello-world`);
-  let message = await response.text();
-  res.render("index.ejs", { message: message })
+  res.render("index.ejs", { message: "Hello from dashboard" });
 })
 
 
