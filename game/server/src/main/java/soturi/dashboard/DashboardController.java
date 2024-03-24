a23755aacd9bac7b6aec45a731c34ec80e84b4e4
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-03-24 20:47:49 +0100

    improvements

--- game/server/src/main/java/soturi/dashboard/DashboardController.java
+++ game/server/src/main/java/soturi/dashboard/DashboardController.java
@@ -2,37 +2,21 @@ package soturi.dashboard;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import soturi.model.Position;
-
-import java.util.List;
 
 @Slf4j
 @Controller
 public class DashboardController {
 
-    private record EnemyDto(String name, int lvl, Position position) { }
-
-    private record PlayerDto(String name, int lvl, Position position) { }
 
     @GetMapping("/")
-    String index(Model model) {
-        EnemyDto enemy = new EnemyDto(
-                "Żaba",
-                3,
-                new Position(50.06783, 19.90868)
-        );
-        model.addAttribute("enemies", List.of(enemy));
-
-
-        PlayerDto player = new PlayerDto(
-                "Student TCS",
-                5,
-                new Position(50.06883, 19.91868)
-        );
-        model.addAttribute("players", List.of(player));
-
+    String index() {
         return "index";
+    }
+
+
+    @GetMapping("/map-view")
+    String mapView() {
+        return "map-view";
     }
 }
