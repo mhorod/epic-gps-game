89d95c0032ef3d527b3aa98c38d499278f082c25
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-05-15 22:07:51 +0200

    Adaptive colors

--- dashboard/src/SpawnAreas.tsx
+++ dashboard/src/SpawnAreas.tsx
@@ -1,10 +1,12 @@
-import { LatLngBoundsExpression, LatLngExpression } from "leaflet";
+import { LatLngExpression } from "leaflet";
 import { MapContainer, TileLayer, Polygon } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Component, useState } from "react";
+import { Component } from "react";
 
-import { Area, PolygonWithDifficulty } from "./model/model";
+import { PolygonWithDifficulty } from "./model/model";
 import { http_path } from "./backend";
+
+import convert from "color-convert";
 
 const position: LatLngExpression = [50.03028264463553, 19.907693170114893];
 
@@ -12,9 +14,51 @@ type MapComponentProps = {
   areas: PolygonWithDifficulty[];
 };
 
-const colors = ["#90be6d", "#f9c74f", "#f3722c", "#f94144"];
+const colors = ["#90be6d", "#f9c74f", "#f3722c", "#f94943"];
+
+const hslColors = colors.map((color) => convert.hex.hsl(color));
+
+class ColorMap {
+  private thresholds: number[] = [];
+  constructor(
+    readonly minDifficulty: number,
+    readonly maxDifficulty: number,
+  ) {
+    const step = (maxDifficulty - minDifficulty) / colors.length;
+    for (let i = 0; i <= colors.length; i++) {
+      const threshold = minDifficulty + step * i;
+      this.thresholds.push(threshold);
+    }
+  }
+
+  color(difficulty: number): string {
+    let i = 0;
+    while (i + 2 < colors.length && difficulty > this.thresholds[i + 1]) i++;
+
+    const lowerThreshold = this.thresholds[i];
+    const upperThreshold = this.thresholds[i + 1];
+
+    const t = (difficulty - lowerThreshold) / (upperThreshold - lowerThreshold);
+    const lowerColor = hslColors[i];
+    const upperColor = hslColors[i + 1];
+
+    const color: [number, number, number] = [
+      lowerColor[0] * (1 - t) + upperColor[0] * t,
+      lowerColor[1] * (1 - t) + upperColor[1] * t,
+      lowerColor[2] * (1 - t) + upperColor[2] * t,
+    ];
+
+    return "#" + convert.hsl.hex(color);
+  }
+}
 
 function MapComponent(props: MapComponentProps) {
+  const difficulties = props.areas.map((area) => area.difficulty);
+  const minDiff = Math.min(...difficulties);
+  const maxDiff = Math.max(...difficulties);
+
+  const colorMap = new ColorMap(minDiff, maxDiff);
+
   return (
     <MapContainer center={position} zoom={13} className="map">
       <TileLayer
@@ -29,7 +73,7 @@ function MapComponent(props: MapComponentProps) {
             latLongs.push([point.latitude, point.longitude]);
           }
 
-          const color = colors[area.difficulty - 1];
+          const color = colorMap.color(area.difficulty);
           return <Polygon positions={latLongs} color={color} />;
         })}
       </>
