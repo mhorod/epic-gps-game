729f922d9d86e467dadb7edd11ad97e27161feee
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-05-18 21:38:48 +0200

    Make clusters of size 1

--- dashboard/src/map/MarkerCluster.tsx
+++ dashboard/src/map/MarkerCluster.tsx
@@ -29,16 +29,25 @@ function getIcon(gfxName: string) {
   });
 }
 
+const maxClusterZoom = 13;
+
+type Point = geojson.Feature<geojson.Point, any>;
+
 function MarkerCluster(props: MarkerClusterProps) {
   const map = useMap();
   const [markers, setMarkers] = useState(
-    L.geoJson(null, { pointToLayer: createClusterIcon }).addTo(map),
+    L.geoJson(null, {
+      pointToLayer: (point: Point, latLng: L.LatLng) => {
+        const zoom = map.getZoom();
+        return createClusterIcon(point, latLng, zoom);
+      },
+    }).addTo(map),
   );
 
   const index = new Supercluster({
     radius: 150,
-    maxZoom: 13,
-    minPoints: 5,
+    maxZoom: maxClusterZoom,
+    minPoints: 1,
   });
   const features: GeoJSON.Feature<geojson.Point, PointProperties>[] =
     props.markers.map((m) => {
@@ -86,34 +95,46 @@ function update(map: Map, index: Supercluster, markers: L.GeoJSON) {
 }
 
 function createClusterIcon(
-  feature: geojson.Feature<geojson.Point, any>,
+  feature: Point,
   latLng: L.LatLng,
+  zoom: number,
 ): L.Layer {
   if (!feature.properties.cluster) {
-    const marker = L.marker(latLng, {
-      icon: getIcon(feature.properties.gfxName),
-    });
-    marker.on("click", feature.properties.onClick);
-    return marker;
+    if (zoom > maxClusterZoom) return createEnemyIcon(feature, latLng);
+    else return createNumberIcon(1, "1", latLng);
   } else {
-    var count = feature.properties.point_count;
-    var size = count < 100 ? "small" : count < 1000 ? "medium" : "large";
-
-    var icon = L.divIcon({
-      html:
-        "<div><span>" +
-        feature.properties.point_count_abbreviated +
-        "</span></div>",
-      className: "marker-cluster marker-cluster-" + size,
-      iconSize: L.point(40, 40),
-    });
-
-    const marker = L.marker(latLng, {
-      icon: icon,
-    });
-
-    return marker;
+    const count = feature.properties.point_count;
+    const text = feature.properties.point_count_abbreviated;
+    return createNumberIcon(count, text, latLng);
   }
+}
+
+function createEnemyIcon(feature: Point, latLng: L.LatLng): L.Layer {
+  const marker = L.marker(latLng, {
+    icon: getIcon(feature.properties.gfxName),
+  });
+  marker.on("click", feature.properties.onClick);
+  return marker;
+}
+
+function createNumberIcon(
+  count: number,
+  text: string,
+  latLng: L.LatLng,
+): L.Layer {
+  var size = count < 100 ? "small" : count < 1000 ? "medium" : "large";
+
+  var icon = L.divIcon({
+    html: "<div><span>" + text + "</span></div>",
+    className: "marker-cluster marker-cluster-" + size,
+    iconSize: L.point(40, 40),
+  });
+
+  const marker = L.marker(latLng, {
+    icon: icon,
+  });
+
+  return marker;
 }
 
 export default MarkerCluster;
