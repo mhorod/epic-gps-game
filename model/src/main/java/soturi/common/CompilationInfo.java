fe745c12dd486998c8edf8b75198ae3baddc999e
Kacper Topolski <kacpertopolski@op.pl>
2024-05-22 21:55:16 +0200

    Version info

---
+++ model/src/main/java/soturi/common/CompilationInfo.java
@@ -0,0 +1,33 @@
+package soturi.common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.Instant;
+import java.util.Optional;
+
+public class CompilationInfo {
+    private CompilationInfo() {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Optional<String> readLineFrom(String file) {
+        try {
+            InputStream is = CompilationInfo.class.getClassLoader().getResourceAsStream(file);
+            return Optional.ofNullable(new BufferedReader(new InputStreamReader(is)).readLine());
+        }
+        catch (Exception ignored) {
+            ignored.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Instant> getCompilationTime() {
+        return readLineFrom("generated/compilation-time").map(Instant::parse);
+    }
+
+    public static Optional<String> getCommitId() {
+        return readLineFrom("generated/commit-id");
+    }
+}
