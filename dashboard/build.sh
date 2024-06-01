ee5cd83a232e167395cbb2fc1401d75d70f7688f
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-06-01 10:41:32 +0200

    Test better build script

--- dashboard/build.sh
+++ dashboard/build.sh
@@ -1,4 +1,11 @@
 npm run build
+STATUS_CODE=$?
+
+if [[ STATUS_CODE != 0 ]]; then 
+  echo "Build failed with exit code $STATUS_CODE"
+  exit $STATUS_CODE
+fi
+
 mv build dashboard
 mkdir -p build/static
 mv dashboard build/static/dashboard
