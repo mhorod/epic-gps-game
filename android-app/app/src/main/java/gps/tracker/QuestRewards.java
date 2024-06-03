5a240f2fb2eeb8c081243ad0eb94500c48b18b27
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-06-03 13:34:24 +0200

    we are getting closer to this being functional

--- android-app/app/src/main/java/gps/tracker/QuestRewards.java
+++ android-app/app/src/main/java/gps/tracker/QuestRewards.java
@@ -1,18 +1,25 @@
 package gps.tracker;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Space;
 
 import androidx.fragment.app.Fragment;
 
 import gps.tracker.databinding.FragmentQuestRewardsBinding;
+import gps.tracker.views.ObjectWithDescription;
+import soturi.model.Item;
+import soturi.model.ItemId;
+import soturi.model.Reward;
 
 public class QuestRewards extends Fragment {
 
     FragmentQuestRewardsBinding binding;
     MainActivity mainActivity;
+    Reward reward;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +33,22 @@ public class QuestRewards extends Fragment {
 
         binding = FragmentQuestRewardsBinding.inflate(getLayoutInflater());
         mainActivity = (MainActivity) getActivity();
+        reward = mainActivity.currentReward;
+
+        ObjectWithDescription xp = new ObjectWithDescription(mainActivity, String.valueOf(reward.xp()), R.drawable.level, 15);
+        xp.setGravity(Gravity.CENTER);
+
+        binding.rewardInfo.addView(xp);
+
+        Space space = new Space(mainActivity);
+        space.setMinimumHeight(80);
+
+        binding.rewardInfo.addView(space);
+
+        for (ItemId item : reward.items()) {
+            Item itemObject = mainActivity.getGameRegistry().getItemById(item);
+            ItemChoice.FullItemInfoSegment itemInfo = new ItemChoice.FullItemInfoSegment(mainActivity, itemObject);
+        }
     }
 
 
